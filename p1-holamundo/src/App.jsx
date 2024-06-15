93e1d08754be04c6e3ee7292d5b76dfda537d2aa
Jonathanbees <jonathanbetancurespinosa@gmail.com>
2024-06-14 22:42:10 -0500

    useState Hook cambiar estado

--- p1-holamundo/src/App.jsx
+++ p1-holamundo/src/App.jsx
@@ -3,17 +3,16 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx';
 
 export function App() {
     const formatUserName = (userName) => `@${userName}`
+    //const otrouser = {isFollowing: true, userName: 'angular', name: 'Angular'}
     return (
         <div className='Users'>
             <TwitterFollowCard 
             formatUserName={formatUserName} //Se pasa la funcion como prop
-            isFollowing 
             userName='reactjs' 
             name='React' />
 
             <TwitterFollowCard 
-            formatUserName={formatUserName} 
-            isFollowing={false} 
+            formatUserName={formatUserName}  
             userName='vuejs' 
             name='Vue' 
             />
--- p1-holamundo/src/TwitterFollowCard.jsx
+++ p1-holamundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,22 @@
-export function TwitterFollowCard({formatUserName, userName, name, isFollowing }) {
-    console.log(isFollowing)
+import { useState } from 'react';
+export function TwitterFollowCard({ formatUserName, userName, name }) {
     //Si se quiere hacer inline css, tiene que ser con {{}}
     //Funcion con template string
     //const avatarTemplate = `https://unavatar.io/${userName}`
+    //console.log(isFollowing, userName, name)
+    //USESTATE
+    const [isFollowing, setIsFollowing] = useState(false)
+    /* Estado inicial de isFollowing, 
+    setIsFollowing es una funcion que cambia el estado de isFollowing, 
+    por lo que UseState es un hook, y usa 2 parametros, 
+    el estado inicial y la funcion que cambia el estado*/
+
+
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
@@ -13,8 +27,10 @@ export function TwitterFollowCard({formatUserName, userName, name, isFollowing }
                 </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={() => {
+                    setIsFollowing(!isFollowing) // Cambia el estado de isFollowing
+                }}>
+                    {text}
                 </button>
             </aside>
         </article>
