448e6e682e7f0407179a45f584ea2cc4cc686152
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-07-08 22:48:46 -0500

    initializing p5

---
+++ p5-technical-test/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+<h1>si</h1>,
+)
