8d77210addb37a5576f4c26713cb3f8d3e3a7b20
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-07-10 23:51:53 -0500

    .

--- p5-technical-test/main.jsx
+++ p5-technical-test/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 const root = createRoot(document.getElementById('app'));
 
-root.render(<h1>si</h1>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
