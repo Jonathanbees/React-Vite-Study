0559abe6e59ee6f872c70afd06f3fe9f2273109a
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2024-07-12 23:37:11 -0500

    first fact from API

--- p5-technical-test/src/app.jsx
+++ p5-technical-test/src/app.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+//const CAT_IMAGE_ENDPOINT = `https://cataas.com/cat/says/${text}`
+const CAT_FACT_ENDPOINT = `https://catfact.ninja/fact`
 export function App () {
-  return (
-    <div>
-      <h1>My App</h1>
-    </div>
-  );
+
+    const [fact, setFact] = useState('lorem ipsum dolor sit amet')
+
+    // fetch a random fact from the API
+    useEffect(() => {
+        fetch(CAT_FACT_ENDPOINT)
+            .then(response => response.json())
+            .then(data => setFact(data.fact))
+    }, [])
+
+    // render the fact
+
+
+    return (
+        <main>
+            <h1>My App</h1>
+            <input type="text" />
+            <button>Submit</button>
+            <h2>Random Cat Fact</h2>
+            <p>{ fact }</p>
+        </main>
+    );
+    
 }
